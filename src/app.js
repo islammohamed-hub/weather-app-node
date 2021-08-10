787a200638937588e0f27710267120af2f8a40c2
Asmaa <asmaakhames.runprof@gmail.com>
2021-08-10 16:48:49 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const forecast = require('./utils/forecast');
+const geocode = require('./utils/geocode');
+
+const app = express();
+
+// Getting path for express config
+const indexPagePath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine
+app.set('view engine','hbs');
+app.set('views',viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(indexPagePath));
+
+app.get('',(req,res) => {
+    res.render('index',{
+        appName: 'Weather App',
+        madeBy: 'Islam Mohamed'
+    })
+});
+
+app.get('/about',(req,res) => {
+    res.render('about',{
+        appName: 'About us',
+        madeBy: 'Islam Mohamed'
+    })
+});
+
+app.get('/help',(req,res) => {
+    res.render('help',{
+        appName: 'Help',
+        message: "Please contact us if you run into any problem",
+        madeBy: 'Islam Mohamed'
+    })
+});
+
+app.get('/weather',(req,res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'Please provide an address'
+        })
+    }
+    geocode(req.query.address,(error , { location , longitude , latitude } = {}) => {
+        if (error) {
+            return res.send({ error }) 
+        }
+        forecast(latitude,longitude, (error, forecastData) => {
+           if (error) {
+                return res.send( {error });
+           }
+           res.send({
+            location,
+            forecastData
+           })
+        })
+    })
+});
+
+app.get('/help/*',(req,res) => {
+    res.render('404page',{
+        errorMessage:'Help articale not found'
+    })
+})
+
+app.get('*',(req,res) => {
+    res.render('404page',{
+        errorMessage:'Page Not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log("server is listing on PORT 3000");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const forecast = (latitude,longitude,callback) => {
+    
+    const url=`http://api.weatherstack.com/current?access_key=b28a4bdf5ec422adcff450d5d61b029f&query=${latitude},${longitude}`;
+    
+    request({ url , json:true} , (error,{body} = {}) => {
+        if (error) {
+            callback("Unable to connect to weather service")
+        }else if (body.error) {
+            callback("Unable to find location!")
+        }else {
+            callback(undefined,`${body.current.weather_descriptions[0]} & the current Temperature is ${body.current.temperature} but it feels like ${body.current.feelslike}`)
+        }
+    })
+}
+
+module.exports=forecast;
